c96dda7d1ebba532423f3628b057903ecc8b8e98
Edgardo <medgardo@gmail.com>
2016-10-14 17:20:25 -0400

    Add authors controller. The controller can read and write to DB using author model.

--- module-04/01/app.js
+++ module-04/01/app.js
@@ -3,7 +3,9 @@ const app = express();
 
 // Load and mount the articles controller
 const articles = require('./controllers/articles');
+const authors = require('./controllers/authors');
 app.use('/articles', articles);
+app.use('/authors', authors);
 
 module.exports = app;
 app.listen(8000);
