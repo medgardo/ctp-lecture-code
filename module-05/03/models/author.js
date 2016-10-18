a4548aa77572111ad6f7332b752c0928fe048214
Edgardo <medgardo@gmail.com>
2016-10-18 15:58:02 -0400

    Modify Author model to pass tests

--- module-05/03/models/author.js
+++ module-05/03/models/author.js
@@ -1,9 +1,29 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
   var Author = sequelize.define('Author', {
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    bio: DataTypes.TEXT
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [0,500],
+        notEmpty: true
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
